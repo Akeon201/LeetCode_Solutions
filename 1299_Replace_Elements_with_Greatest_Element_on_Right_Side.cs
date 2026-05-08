c903f60c4a7d209566a69e4cfca12016143db9ba
Kenyon Leblanc <leblanckenyon@gmail.com>
2026-05-08 02:08:15 -0400

    Add solutions for LeetCode 1299 and 27; stub for 1929

---
+++ 1299_Replace_Elements_with_Greatest_Element_on_Right_Side.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int[] ReplaceElements(int[] arr)
+    {
+        int maxNum = arr[arr.Length - 1];
+        arr[arr.Length - 1] = -1;
+        int indexValue;
+
+        for (int i = arr.Length - 2; i >= 0; i--)
+        {
+            indexValue = arr[i];
+            arr[i] = maxNum;
+            maxNum = Math.Max(maxNum, indexValue);
+        }
+
+        return arr;
+    }
+}
