ec470977edf4c550a8e0d2aa1d591d3d6c260261
Kenyon Leblanc <leblanckenyon@gmail.com>
2026-05-08 00:52:39 -0400

    Add project files.

---
+++ 485_Max_Consecutive_Ones.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode_Solutions
+{
+    public class Solution
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int count = 0;
+            int maxCount = 0;
+
+            foreach (int num in nums)
+            {
+                if (num == 1)
+                    count++;
+                else
+                {
+                    maxCount = Math.Max(maxCount, count);
+                    count = 0;
+                }
+            }
+
+            return maxCount;
+        }
+    }
+}
